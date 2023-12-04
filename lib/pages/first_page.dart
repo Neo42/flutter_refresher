b6a045588bb1894b822d8bfcf4b1fce9d66b05c1
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-12-04 15:45:33 +1030

    feat: ✨ add bottom navbar

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,41 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter/pages/home_page.dart';
+import 'package:learn_flutter/pages/profile_page.dart';
 import 'package:learn_flutter/pages/settings_page.dart';
 
-class FirstPage extends StatelessWidget {
+class FirstPage extends StatefulWidget {
   const FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  int _selectedIndex = 0;
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List _pages = [
+    // homepage
+    HomePage(),
+    // profilepage
+    ProfilePage(),
+    // settingspage
+    SettingsPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('1st Page')),
-      drawer: Drawer(
-        backgroundColor: Colors.deepPurple[100],
-        child: Column(
-          children: [
-            DrawerHeader(
-              child: Icon(Icons.favorite),
-            ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomBar,
+        items: [
+          // home
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
 
-            // home page list title
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("H O M E"),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, '/homepage');
-              },
-            ),
-// settings page list title
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("S E T T I N G S"),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, '/settingspage');
-              },
-            ),
-          ],
-        ),
+          // profile
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+
+          // settings
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
       ),
     );
   }
